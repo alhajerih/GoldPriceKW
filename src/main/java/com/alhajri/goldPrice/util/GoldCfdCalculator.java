91949a9f56584a2d76c451aed8922d679e35d644
Hamad Alhajri <alhajerih@hotmail.com>
2026-02-14 15:26:16 +0300

    fix the broker factor rate

--- src/main/java/com/alhajri/goldPrice/util/GoldCfdCalculator.java
+++ src/main/java/com/alhajri/goldPrice/util/GoldCfdCalculator.java
@@ -3,7 +3,7 @@ package com.alhajri.goldPrice.util;
 public class GoldCfdCalculator {
 
     private static final double GRAM_TO_OUNCE = 31.1035;
-    private static final double BROKER_FACTOR = 0.994;
+    private static final double BROKER_FACTOR = 0.992;
 
     /**
      * Calculate CFD using dynamic USD per KWD
